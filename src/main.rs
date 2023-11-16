95a66ebf7d445c315ef81a5dca608dd408ead284
Nick McSpadden <nmcspadden@gmail.com>
2023-11-16 11:07:54 +0000

    Extract paths from hardcoded pkg

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,17 @@
+// For development purposes
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+
+
 // This was written for inspec 5.21.29+
+use autoscorecard_rs::extract_contents;
 
-/*
-    inspec init profile <profile name>
-    inspec check <path to folder>
-    1. Edit inspec.yml
-    2. Extract BOM from .pkg file
-    3. Create new control with files and modes 
-
-*/
-
-fn extract_contents() {
-    // extract or examine payload from archive
-}
-
-fn extract_pkg_payload() {
-    // extract payload from pkg file
-}
-
-fn extract_zip_payload() {
-    // extract payload from zip file
-}
-
-fn extract_dmg_payload() {
-    // extract payload from dmg file
-}
-
-fn extract_rpm_payload() {
-    // extract payload from rpm file
-}
-
-fn create_inspec_profile() {
-    // create the inspec init here
-}
-
-fn edit_inspec_yml() {
-    // edit the inspec.yml here
-}
-
-fn edit_inspec_readme() {
-    // edit the inspec readme here
-}
-
-fn create_control() {
-    // create the control for the payload files here
-}
-
-fn check_control() {
-    // Run inspec check on finished control to validate
-}
 fn main() {
-    println!("Hello, world!");
+    extract_contents();
     /*
-    1. extract_contents() - Determine type and extract archive/obtain BOM 
-    2. extract_<type>_payload() - extract archive/obtain BOM 
+    1. extract_contents() - Determine type and extract archive/obtain BOM
+    2. extract_<type>_payload() - extract archive/obtain BOM
     3. create_inspec_profile() - init new inspec profile
     4. edit_inspec_yml() - edit yml with package data
     5. edit_inspec_readme() - edit readme with package data
